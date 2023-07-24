2ed475ff9548a1908a62183ea61e16025e6c84c1
Rajesh <logicraju@gmail.com>
2023-07-24 12:47:05 +0530

    web-interface

--- src/bringup/launch/automated_delivery/start_http_server.sh
+++ src/bringup/launch/automated_delivery/start_http_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Change to the directory where you want to serve files (replace "<your_package_name>/www" with the appropriate path)
-cd "$(rospack find bringup)/launch/automated_delivery/"
+cd "/home/rajesh/ROS_Workspaces/ROS1/BumbleBot_WS/src"
 
 # Start the HTTP server on port 8000
 python3 -m http.server 8000
